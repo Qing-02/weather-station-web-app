ef877aaf45f2378d665be07af60d6315bd03dd7b
Qing Qin <qinqing044@gmail.com>
2026-02-25 07:10:01 +0000

    Real-Time Environmental Monitoring change onti Wether Station

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -7,7 +7,7 @@ export function Hero() {
       <div className="container px-4 md:px-6 z-10">
         <div className="space-y-4">
           <h1 className="text-4xl font-extrabold tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl/none">
-            Real-Time Environmental Monitoring
+            Weather Station
           </h1>
           <p className="mx-auto max-w-[700px] text-muted-foreground md:text-xl">
             Harnessing the power of IoT and cloud technology to deliver precise, live weather data right to your screen.
